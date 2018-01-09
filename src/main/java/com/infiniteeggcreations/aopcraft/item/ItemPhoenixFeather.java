7aaabd76e0e143fc02c76a816c867c566c4bb5d4
Lahermosa1 <30783721+lahermosa1@users.noreply.github.com>
2018-01-09 17:20:24 -0600

    added ItemPhoenixFeather
    Successfully added Phoenix Feather item and rendered texture. Runs.

---
+++ src/main/java/com/infiniteeggcreations/aopcraft/item/ItemPhoenixFeather.java
@@ -0,0 +1,11 @@
+package com.infiniteeggcreations.aopcraft.item;
+
+import net.minecraft.item.Item;
+
+public class ItemPhoenixFeather extends Item {
+
+    public ItemPhoenixFeather(String name) {
+        setUnlocalizedName(name);
+        setRegistryName(name);
+    }
+}
