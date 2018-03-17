8de563c90eda7116b70f1b162a170a74dadc59e4
Lahermosa1 <30783721+lahermosa1@users.noreply.github.com>
2018-03-17 06:29:28 -0500

    Add files via upload

---
+++ src/main/java/com/infiniteeggcreations/aopcraft/entity/projectile/EntitySmallFlame.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2018. Age of the Phoenix and B. H. Gorden.  All rights reserved.
+ */
+
+package com.infiniteeggcreations.aopcraft.entity.projectile;
+
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class EntitySmallFlame extends EntityThrowable
+{
+    public EntitySmallFlame(World worldIn, EntityLivingBase throwerIn)
+    {
+        super(worldIn, throwerIn);
+    }
+
+    /**
+     * Handler for {@link World#setEntityState}
+     */
+    @SideOnly(Side.CLIENT)
+    public void handleStatusUpdate(byte id)
+    {
+        if (id == 3)
+        {
+            double d0 = 0.08D;
+
+            for (int i = 0; i < 8; ++i)
+            {
+                this.world.spawnParticle(EnumParticleTypes.ITEM_TAKE, this.posX, this.posY, this.posZ, ((double)this.rand.nextFloat() - 0.5D) * 0.08D, ((double)this.rand.nextFloat() - 0.5D) * 0.08D, ((double)this.rand.nextFloat() - 0.5D) * 0.08D, Item.getIdFromItem(Items.FIRE_CHARGE));
+            }
+        }
+    }
+
+    /**
+     * Called when this EntityThrowable hits a block or entity.
+     */
+    protected void onImpact(RayTraceResult result)
+    {
+        setOnFireFromLava();
+    }
+}
